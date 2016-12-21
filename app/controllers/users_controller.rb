85d024b2c2a88eaf4f7fcba7a38f2e6835951766
dgilbs <danielgilberg@gmail.com>
2016-12-21 17:46:27 -0500

    begins work on user new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+  end
+
+  def create
+    byebug
+    x=1
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :position, :bio, :gender)
   end
 end
